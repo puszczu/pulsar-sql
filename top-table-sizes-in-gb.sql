fa2a74c1fb6a881ed945b2ea7b0808b36bebc886
puszczu <49039750+puszczu@users.noreply.github.com>
2023-10-23 12:09:57 +0200

    Update top-table-sizes-in-gb.sql
    second query to calculate real blob sizes

--- top-table-sizes-in-gb.sql
+++ top-table-sizes-in-gb.sql
@@ -24,3 +24,15 @@ SELECT owner, table_name, TRUNC(sum(bytes) / 1024 / 1024 / 1024) GB
  GROUP BY table_name, owner
 HAVING SUM(bytes) / 1024 / 1024 > 10 /* Ignore really small tables */
  ORDER BY SUM(bytes) desc
+
+
+ select 'select ''' || c1.TABLE_NAME ||
+        ''' as table_name, round(sum(dbms_lob.getlength(d1.' ||
+        c1.COLUMN_NAME || ')) / 1024 / 1024 / 1024, 2) as size_in_gb from ' ||
+        c1.owner || '.' || c1.TABLE_NAME || ' d1 union all ' as sql_text
+   from all_tab_cols c1
+  inner join all_tables tab
+     on tab.TABLE_NAME = c1.table_name
+    and tab.OWNER = c1.owner
+  where c1.owner in ('PULSAR', 'LMT', 'REST', 'FAKTURY', 'IBSI_FK', 'ZAM')
+    and c1.DATA_TYPE = 'BLOB'
